858a53aa802398dc9a1b49ee11bbc000add0767f
moudroff <moudroff>
2016-10-10 13:08:56 +0300

    работа с массивом

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -11,6 +11,8 @@ namespace Array
         static void Main(string[] args)
         {
             int n;
+            int kol = 0, max = -101, i1=-1, i2=-1;
+
             Console.WriteLine("размер");
             n = int.Parse(Console.ReadLine());
             int[] a = new int[n], b = new int[n];
@@ -20,23 +22,34 @@ namespace Array
                 //  a[i] = int.Parse(Console.ReadLine());
                 a[i] = r.Next(-100,100);
 
-            int sum = 0, min = a[0], max = a[0];
             for (int i = 0; i < n; i++)
             {
-                if (min > a[i])
-                    min = a[i];
                 if (max < a[i])
+                {
                     max = a[i];
-                sum += a[i];
-                Console.Write($"a[{i}]={a[i]} ");
+                    kol = 1;
+                    i1 = i2 = i;
+                }
+                else if (max==a[i])
+                {
+                    kol++;
+                    i2 = i;
+                }
+
             }
-            Console.WriteLine($"\nSumma = {sum} Max = {max} Min = {min}");
+            Console.WriteLine($"\n Max = {max} Kol = {kol} i1={i1} i2={i2}");
 
+            int k = 0;
+            bool fl = true;
+            for (int i = 0; i < n-1; i++)
+            {
+                if (a[i] * a[i + 1] < 0 || (a[i]==0 && a[i+1]<0) 
+                    || (a[i]<0 && a[i+1]==0))
+                    k++;
+                if (a[i] > a[i + 1])
+                    fl = false;
 
-
-
-
-
+            }
 
 
 
