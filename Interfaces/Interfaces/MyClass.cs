7fbaeffcdbf5218e1e5c641a0a1986737338ebbc
moudroff <moudroff>
2016-10-19 16:29:45 +0300

    интерфейсы

---
+++ Interfaces/Interfaces/MyClass.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    abstract class Vehicle
+    {
+        private int curSpeed, maxSpeed;
+
+        public abstract void SpeedUp();
+        public abstract void SpeedDown();
+
+    }
+
+
+    interface IArOper
+    {
+        int Sum();
+        int Minus();
+        int Prod();
+        int Div();
+    }
+
+    class TwoInt:IArOper
+    {
+        private int a;
+        private int b;
+
+        public TwoInt(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"TwoInt: {a} {b}");
+        }
+
+        public int Div()
+        {
+            return a / b;
+        }
+
+        public int Minus()
+        {
+            return a - b; 
+        }
+
+        public int Prod()
+        {
+            return a * b; 
+        }
+
+        public int Sum()
+        {
+            return a + b; 
+        }
+    }
+
+    class ThreeInt : IArOper
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public ThreeInt(int a, int b, int  c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"ThreeInt: {a} {b} {c}");
+        }
+
+        public int Div()
+        {
+            return a / b / c;
+        }
+
+        public int Minus()
+        {
+            return a - b - c;
+        }
+
+        public int Prod()
+        {
+            return a * b * c;
+        }
+
+        public int Sum()
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IArOper> li = new List<IArOper>();
+
+            
+            li.Add(new TwoInt(6, 3));
+            li.Add(new ThreeInt(24, 4, 2));
+
+            foreach(var l in li)
+            {
+                Console.WriteLine(l);
+                Console.WriteLine($"Summa = {l.Sum()}");
+                Console.WriteLine($"Разн = {l.Minus()}");
+                Console.WriteLine($"Произв = {l.Prod()}");
+                Console.WriteLine($"Деление = {l.Div()}");
+                Console.WriteLine("**********************************");
+            }
+
+            
+
+
+        }
+    }
+}
