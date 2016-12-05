9e655a5d6cf1d4a9b40a8cb5940a8eb723e75160
moudroff <moudroff>
2016-12-05 17:59:33 +0300

    интерфейс

---
+++ Inter/Inter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inter
+{
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+    }
+
+    interface IFigure
+    {
+        void Move(double x, double y);
+        void Scale(double angle);
+        double Area();
+        bool IsEqual(IFigure f);
+        bool IsContain(IFigure f);
+    }
+
+    public class Treug : IFigure
+    {
+        Point  
+        public double Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsContain(IFigure f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEqual(IFigure f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scale(double angle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
